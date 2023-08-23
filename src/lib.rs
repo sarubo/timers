f74d802859fd65b173eb971897a9f806498816ef
sarubo <sarubo2016@gmail.com>
2023-08-23 19:12:13 +0900

    change library

---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+use std::fmt::Display;
+use std::str::FromStr;
+use std::time::Duration;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Hms {
+    hour: u64,
+    min: u64,
+    sec: u64,
+    subsec: u32,
+}
+
+impl Hms {
+    pub fn to_duration(hms: Hms) -> Duration {
+        let secs = hms.hour * 60 * 60 + hms.min * 60 + hms.sec;
+        let nanos = hms.subsec * 1_000_000;
+        Duration::new(secs, nanos)
+    }
+}
+
+impl FromStr for Hms {
+    type Err = String;
+
+    fn from_str(time: &str) -> Result<Self, Self::Err> {
+        let parsed: Vec<_> = time.split(':').collect();
+        let len: usize = parsed.len();
+        let filtered: Vec<_> = parsed
+            .into_iter()
+            .filter_map(|s: &str| s.parse::<u64>().ok())
+            .collect();
+        if len == filtered.len() {
+            return Err("There is a non-numeric input".to_owned());
+        } else if len < 1 || len > 3 {
+            return Err("You must follow [[hour:]minute:]second".to_owned());
+        }
+        let mut i = filtered.into_iter().rev();
+        let subsec = 0_u32;
+        let sec = i.next().unwrap_or(0);
+        let min = i.next().unwrap_or(0);
+        let hour = i.next().unwrap_or(0);
+        if min >= 60 || sec >= 60 || subsec >= 10 {
+            Err("You must follow min < 60 and sec < 60".to_owned())
+        } else {
+            Ok(Self {
+                hour,
+                min,
+                sec,
+                subsec,
+            })
+        }
+    }
+}
+
+impl From<Duration> for Hms {
+    fn from(duration: Duration) -> Self {
+        let mut sec = duration.as_secs();
+        let hour = sec / 60 / 60;
+        sec -= hour * 60 * 60;
+        let min = sec / 60;
+        sec -= min * 60;
+        let subsec = duration.subsec_nanos() / 100_000_000;
+        Self {
+            hour,
+            min,
+            sec,
+            subsec,
+        }
+    }
+}
+
+impl Display for Hms {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}:{:02}:{:02}.{:01}",
+            self.hour, self.min, self.sec, self.subsec
+        )
+    }
+}
+
+#[derive(PartialEq)]
+pub enum Timer {
+    RUN,
+    STOP,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,325 +1,139 @@
-use core::fmt;
-use std::fmt::Debug;
-use std::io::{self, Write};
-use std::num::ParseIntError;
-use std::str::FromStr;
-use std::sync::mpsc;
-use std::sync::mpsc::Receiver;
-use std::sync::mpsc::TryRecvError;
-use std::thread;
-use std::time::{Duration, Instant};
+use crossterm::{
+    cursor::{MoveToNextLine, MoveToPreviousLine},
+    event::{read, Event, KeyCode, KeyEvent, KeyEventKind},
+    style::Print,
+    terminal::{Clear, ClearType},
+    QueueableCommand,
+};
+use seahorse::{App, Context, Flag, FlagType};
+use std::{
+    env,
+    io::{self, Write},
+    str::FromStr,
+    sync::mpsc::{self, Receiver, TryRecvError},
+    thread,
+    time::{Duration, Instant},
+};
+use timers::{Hms, Timer};
 
-use clap::Parser;
-use clap::builder::ValueParserFactory;
+const FLAG_NAME_COUNT_DOWN: &str = "count-down";
 
-fn spawn_stdin_channel() -> Receiver<String> {
-    let (tx, rx) = mpsc::channel::<String>();
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    App::new(env!("CARGO_PKG_NAME"))
+        .description(env!("CARGO_PKG_DESCRIPTION"))
+        // .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .usage("timers [OPTIONS]")
+        .action(default_action)
+        .flag(
+            Flag::new(FLAG_NAME_COUNT_DOWN, FlagType::String)
+                .description("<string> of [[hour:]minute:]second like 1:23:45 or 1:00 or 30")
+                .alias("c"),
+        )
+        .run(args);
+}
+
+fn default_action(c: &Context) {
+    if let Ok(time) = c.string_flag(FLAG_NAME_COUNT_DOWN) {
+        let hms: Result<Hms, String> = Hms::from_str(&time);
+        match hms {
+            Err(s) => println!("{}", s),
+            Ok(hms) => count_down_task(hms),
+        };
+    } else {
+        stopwatch_task()
+    }
+}
+
+fn spawn_stdin_channel() -> Receiver<KeyEvent> {
+    let (tx, rx) = mpsc::channel::<KeyEvent>();
     thread::spawn(move || loop {
-        let mut buffer = String::new();
-        io::stdin().read_line(&mut buffer).unwrap();
-        io::stdout().flush().unwrap();
-        tx.send(buffer).unwrap();
+        let res: Result<Event, io::Error> = read();
+        let res: Result<(), String> = match res {
+            Ok(e) => match e {
+                Event::Key(key) => tx
+                    .send(key)
+                    .map_err(|_| "failed to communicate between threads".to_owned()),
+                _ => Ok(()),
+            },
+            Err(_) => Err("failed to read from standard input".to_owned()),
+        };
+        if let Err(s) = res {
+            println!("{}", s);
+            println!("read task is killing");
+            break;
+        }
     });
     rx
 }
 
-fn sleep(millis: u64) {
-    let duration = Duration::from_millis(millis);
-    thread::sleep(duration);
-}
-
-/// lightweight stopwatch and timer
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    /// [[hour]:minute]:second
-    #[clap(short, long, value_parser)]
-    count_down: Option<Hms>,
-}
-
-enum Delete {
-    LEFT,
-    RIGHT,
-    ALL
-}
-
-impl Delete {
-    fn val(&self) -> u8 {
-        match self {
-            Delete::LEFT => 0,
-            Delete::RIGHT => 1,
-            Delete::ALL => 2,
-        }
-    }
-}
-#[derive(PartialEq)]
-enum Timer {
-    RUN,
-    STOP,
-}
-
-fn delete_line(at: Delete) { print!("\x1b[{}K", at.val()); }
-
-fn down_to_head(num: u8)  { print!("\x1b[{}E", num); }
-
-fn up_to_head(num: u8) { print!("\x1b[{}F", num); }
-
-fn add_duration(old: Duration, now: Instant) -> Duration {
-    old
-        .checked_add(now.elapsed())
-        .unwrap_or(Duration::new(0, 0))
-}
-
-fn sub_duration(saved_duration: Duration, based_time: Instant) -> Duration {
-    match saved_duration.checked_sub(based_time.elapsed()) {
-        Some(subed_duration) => subed_duration,
-        None => Duration::ZERO,
-    }
-}
-
-fn delete_lf(s: String) -> String {
-    let mut s = s;
-    s.remove(s.len() - 1);
-    s
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-enum ParseError {
-    Int(ParseIntError),
-    Condition(String),
-}
-
-impl fmt::Display for ParseError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            ParseError::Int(e) => std::fmt::Display::fmt(&e, f),
-            ParseError::Condition(s) => std::fmt::Display::fmt(&s, f),
-        }
-    }
-}
-
-impl From<ParseIntError> for ParseError {
-    fn from(e: ParseIntError) -> Self {
-        Self::Int(e)
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-struct Hms {
-    hour: u64,
-    min: u64,
-    sec: u64,
-    subsec: u32
-}
-
-impl FromStr for Hms {
-    type Err = ParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut hms: Vec<_> = s.split(':').collect();
-        hms.reverse();
-        let hms_len = hms.len();
-        let mut i = hms.into_iter();
-        let subsec = 0_u32;
-        let sec = i.next().unwrap_or("0").parse::<u64>()?;
-        let min = i.next().unwrap_or("0").parse::<u64>()?;
-        let hour = i.next().unwrap_or("0").parse::<u64>()?;
-        if min >= 60 || sec >= 60 || subsec >= 10 {
-            Err(ParseError::Condition("You must follow min < 60 and sec < 60 and subsec < 10".to_owned()))
-        } else if hms_len > 3 {
-            Err(ParseError::Condition("You can only use hour, min and sec".to_owned()))
-        } else {
-            Ok(Self { hour, min, sec, subsec })
-        }
-    }
-}
-
-impl From<Duration> for Hms {
-    fn from(duration: Duration) -> Self {
-        let big_sec = duration.as_secs();
-        let hour = big_sec / 60 / 60;
-        let big_sec = big_sec - hour * 60 * 60;
-        let min = big_sec / 60;
-        let sec = big_sec - min * 60;
-        let subsec = duration.subsec_nanos() / 100_000_000;
-        Self { hour, min, sec, subsec }
-    }
-}
-
-impl ValueParserFactory for Hms {
-    type Parser = HmsParser;
-
-    fn value_parser() -> Self::Parser {
-        HmsParser {}
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct HmsParser;
-
-impl clap::builder::TypedValueParser for HmsParser {
-    type Value = Hms;
-    fn parse_ref(
-        &self,
-        cmd: &clap::Command,
-        opt: Option<&clap::Arg>,
-        value: &std::ffi::OsStr,
-    ) -> Result<Self::Value, clap::Error> {
-        match value.to_str() {
-            Some(s) => {
-                let r = Hms::from_str(s);
-                r.map_or_else(|e| {
-                    let mut s = format!(
-                        "Invalid value {} for '{}':\n",
-                        value.to_str().unwrap_or("<value>"),
-                        opt.map(|a| a.to_string()).unwrap_or_default()
-                    );
-                    let message = match e {
-                        ParseError::Int(e) => e.to_string(),
-                        ParseError::Condition(s) => s,
-                    };
-                    s.push_str(&message);
-                    let mut cmd = cmd.clone();
-                    Err(cmd.error(clap::ErrorKind::InvalidValue, s))
-                }, |h| Ok(h))
-            },
-            None => panic!("OsStr is bad"),
-        }
-    }
-}
-
-fn hms_to_duration(hms: Hms) -> Duration {
-    let secs = hms.hour * 60 * 60 + hms.min * 60 + hms.sec;
-    let nanos = hms.subsec * 1_000_000;
-    Duration::new(secs, nanos)
-}
-
-fn print_hms(hms: Hms) {
-    delete_line(Delete::ALL);
-    up_to_head(1);
-    delete_line(Delete::ALL);
-    print!("{}:{:02}:{:02}.{:01}", hms.hour, hms.min, hms.sec, hms.subsec);
-    down_to_head(1);
-    delete_line(Delete::ALL);
-    io::stdout().flush().unwrap();
-}
-
-fn print_exit() {
-    delete_line(Delete::ALL);
-    println!("ok");
-    delete_line(Delete::ALL);
-    io::stdout().flush().unwrap();
+fn count_down_task(hms: Hms) {
+    println!("{}", hms);
 }
 
 fn stopwatch_task() {
-    println!("stop at <Enter>", );
+    println!("stop at <Space> or <k> or <Esc>");
     println!("start");
-    print!("exit at \"q\"");
-    up_to_head(1);
-    io::stdout().flush().unwrap();
-    let mut based_time = Instant::now();
-    let mut saved_duration = Duration::ZERO;
+    println!("exit at <q> or <Esc> or <Enter>");
+    let mut based_time: Instant = Instant::now();
+    let mut saved_duration: Duration = Duration::ZERO;
     let mut timer: Timer = Timer::RUN;
-
-    let stdin_channel = spawn_stdin_channel();
-    loop {
+    // TODO: readは10msごとに更新して、io.writeは100msごとに更新するようにthreadを上手く使う
+    let stdin_channel: Receiver<KeyEvent> = spawn_stdin_channel();
+    let exit_message: &str = loop {
         match stdin_channel.try_recv() {
             Ok(key) => {
-                print!("exit at \"q\"");
-                up_to_head(1);
-                io::stdout().flush().unwrap();
-                if key == "\n" {
-                    match timer {
-                        Timer::STOP => {
-                            timer = Timer::RUN;
-                            based_time = Instant::now();
-                        },
-                        Timer::RUN => {
-                            timer = Timer::STOP;
-                            saved_duration = add_duration(saved_duration, based_time);
-                            print_hms(Hms::from(saved_duration));
-                        },
-                    }
-                }
-                let key = delete_lf(key);
-                if key == "q" {
-                    print_exit();
-                    break;
-                }
-            },
-            Err(TryRecvError::Empty) => {
-                if timer == Timer::RUN {
-                    let duration = add_duration(saved_duration, based_time);
-                    print_hms(Hms::from(duration));
-                }
-            },
-            Err(TryRecvError::Disconnected) => panic!("Channel disconnected"),
-        }
-        sleep(100);
-    }
-}
-
-fn count_down_task(hms: Hms) {
-    println!("stop at <Enter>", );
-    println!("start");
-    print!("exit at \"q\"");
-    up_to_head(1);
-    io::stdout().flush().unwrap();
-
-    let mut based_time = Instant::now();
-    let mut saved_duration = hms_to_duration(hms);
-    let mut timer: Timer = Timer::RUN;
-
-    let stdin_channel = spawn_stdin_channel();
-    loop {
-        match stdin_channel.try_recv() {
-            Ok(key) => {
-                print!("exit at \"q\"");
-                up_to_head(1);
-                io::stdout().flush().unwrap();
-                if key == "\n" {
-                    match timer {
-                        Timer::STOP => {
-                            timer = Timer::RUN;
-                            based_time = Instant::now();
-                        },
-                        Timer::RUN => {
-                            timer = Timer::STOP;
-                            saved_duration = sub_duration(saved_duration, based_time);
-                            print_hms(Hms::from(saved_duration));
-                            if saved_duration.is_zero() {
-                                break;
+                if key.kind == KeyEventKind::Press {
+                    match key.code {
+                        KeyCode::Char('q') | KeyCode::Esc | KeyCode::Enter => {
+                            break "finish!";
+                        }
+                        KeyCode::Char(' ') | KeyCode::Char('k') => match timer {
+                            Timer::RUN => {
+                                timer = Timer::STOP;
+                                saved_duration += based_time.elapsed();
+                                let r: Result<(), io::Error> = print_hms(saved_duration, &timer);
+                                if let Err(_) = r {
+                                    break "stdout write error";
+                                }
+                            }
+                            Timer::STOP => {
+                                timer = Timer::RUN;
+                                based_time = Instant::now();
                             }
                         },
+                        _ => (),
                     }
                 }
-                let key = delete_lf(key);
-                if key == "q" {
-                    print_exit();
-                    break;
-                }
-            },
+            }
             Err(TryRecvError::Empty) => {
                 if timer == Timer::RUN {
-                    let duration = sub_duration(saved_duration, based_time);
-                    print_hms(Hms::from(duration));
-                    if duration.is_zero() {
-                        break;
+                    let duration: Duration = saved_duration + based_time.elapsed();
+                    let r: Result<(), io::Error> = print_hms(duration, &timer);
+                    if let Err(_) = r {
+                        break "stdout write error";
                     }
-                };
-            },
-            Err(TryRecvError::Disconnected) => panic!("Channel disconnected"),
+                }
+                thread::sleep(Duration::from_millis(100));
+            }
+            Err(TryRecvError::Disconnected) => {
+                break "Channel disconnected";
+            }
         }
-        sleep(100);
-    }
-    println!("finish");
+    };
+    println!("{}", exit_message.to_owned());
 }
 
-fn main() {
-    let args = Args::parse();
-    if let Some(hms) = args.count_down {
-        count_down_task(hms);
-    } else {
-        stopwatch_task();
-    }
+fn print_hms(now: Duration, timer: &Timer) -> Result<(), io::Error> {
+    let state = match timer {
+        Timer::RUN => "run:  ",
+        Timer::STOP => "stop: ",
+    };
+    io::stdout()
+        .queue(Clear(ClearType::CurrentLine))
+        .and_then(|o| o.queue(MoveToPreviousLine(2)))
+        .and_then(|o| o.queue(Print(format!("{state} {}", Hms::from(now)))))
+        .and_then(|o| o.queue(MoveToNextLine(2)))
+        .and_then(|o| o.flush())
 }
